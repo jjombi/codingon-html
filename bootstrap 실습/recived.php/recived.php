0dd750e17b5ed3d7a1b59bb79f9296fbf7cc9f63
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2021-12-29 21:28:33 +0900

    form

---
+++ bootstrap 실습/recived.php/recived.php
@@ -0,0 +1,18 @@
+	
+<html>
+ <head>
+  <title>
+   form태그 공부
+  </title>
+ </head>
+ 
+ <body>
+ <?
+  echo $_POST["first_text"];
+  
+  echo "<br>";
+  
+  echo $_POST["second_text"];
+ ?>
+ </body>
+</html>
